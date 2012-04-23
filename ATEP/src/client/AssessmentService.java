5581b7030fff659b74124e91b53c8b382d44c1c7
Helen Wauck <hwauck@gustavus.edu>
2012-04-23 11:58:06 -0500

    Helen - added client, server classes from SampleWebApplication

---
+++ ATEP/src/client/AssessmentService.java
@@ -0,0 +1,12 @@
+package client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+* The client side stub for the RPC service.
+*/
+@RemoteServiceRelativePath("greet")
+public interface AssessmentService extends RemoteService {
+String atepServer(String name) throws IllegalArgumentException;
+}
---
+++ ATEP/src/client/AssessmentServiceAsync.java
@@ -0,0 +1,11 @@
+package client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface AssessmentServiceAsync {
+	void atepServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
